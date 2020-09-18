79fa5f3b61beed5221f07c3b87d0d4fca862611f
Mick Evans <codemastermick@gmail.com>
2020-09-18 13:06:41 -0400

    feat: add support for automated role management

--- monitors/Social/xp.js
+++ monitors/Social/xp.js
@@ -38,7 +38,7 @@ module.exports = class extends Monitor {
         // Calculate the next value for experience. This is where we need to check the channel, content, etc
         const nextValue = message.author.settings.get('experience') + 1;
         const points_today = message.author.settings.get('points_today') + 1;
-
+        
         if(points_today>=25)return;
 
         // Cache the current level.
@@ -50,6 +50,38 @@ module.exports = class extends Monitor {
         // Update the user's configuration entry by adding 1 to it, and update the level also.
         await message.author.settings.update([['experience', nextValue]]);
         await message.author.settings.update([['points_today', points_today]]);
+
+        if(nextValue>=1000){
+            // add diamond 741282301081550988
+            (await message.guild.members.fetch(message.author.id)).roles.add('741282301081550988');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741283986206359573');
+        }else if(nextValue>=800){
+            // add gold 741283986206359573
+            (await message.guild.members.fetch(message.author.id)).roles.add('741283986206359573');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741282301081550988');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741283768178049074');
+        }else if(nextValue>=600){
+            // add silver 741283768178049074
+            (await message.guild.members.fetch(message.author.id)).roles.add('741283768178049074');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741283986206359573');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741283370259972176');
+        }else if(nextValue>=400){
+            // add bronze 741283370259972176
+            (await message.guild.members.fetch(message.author.id)).roles.add('741283370259972176');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741283768178049074');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741282248682242091');
+        }else if(nextValue>=200){
+            // add wood 741282248682242091
+            (await message.guild.members.fetch(message.author.id)).roles.add('741282248682242091');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741283370259972176');
+        }else{
+            // remove all roles
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741282301081550988');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741283986206359573');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741283768178049074');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741283370259972176');
+            (await message.guild.members.fetch(message.author.id)).roles.remove('741282248682242091');
+        }
         // await message.author.settings.update([['experience', nextValue], ['level', nextLevel]]);
 
         // If the current level and the next level are not the same, then it has increased, and you can send the message.
